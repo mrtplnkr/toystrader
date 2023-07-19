14008d96d9e3d4c204bdeac703acf9b44680881b
martin <m.cepauskas@varutis.lt>
2023-07-19 11:33:29 +0300

    nice user menu

--- src/App.js
+++ src/App.js
@@ -1,18 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
 import Auth from './components/auth';
-import { auth } from "./firebase-config";
 
 function App() {
+
 
   return (
     <div className="App">
       <header className="App-header">
+        <Auth />
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        {auth ? <Auth /> : null }
+        
         <a
           className="App-link"
           href="https://reactjs.org"
@@ -25,5 +26,26 @@ function App() {
     </div>
   );
 }
+
+// let AuthContext = React.createContext<AuthContextType>(undefined);
+
+// function useAuth() {
+//   return React.useContext(AuthContext);
+// }
+
+// function RequireAuth({ children }) {
+//   let auth = useAuth();
+//   let location = useLocation();
+
+//   if (!auth.user) {
+//     // Redirect them to the /login page, but save the current location they were
+//     // trying to go to when they were redirected. This allows us to send them
+//     // along to that page after they login, which is a nicer user experience
+//     // than dropping them off on the home page.
+//     return <Navigate to="/login" state={{ from: location }} replace />;
+//   }
+
+//   return children;
+// }
 
 export default App;
--- src/components/auth.js
+++ src/components/auth.js
@@ -1,9 +1,34 @@
 //https://toystrader-a494f.firebaseapp.com/__/auth/handler
-
+import React, { useEffect, useState } from "react";
 import { auth, googleProvider, facebookProvider } from "../firebase-config";
-import { signInWithPopup } from "firebase/auth";
+import { signInWithPopup, signOut } from "firebase/auth";
 
 function Auth() {
+
+  const [loading, setLoading] = useState(false);
+  const [user, setUser] = useState(undefined);
+
+  const [menu, openMenu] = useState(false);
+
+  useEffect(() => {
+    auth.onAuthStateChanged(async (res) => {
+      setUser({ displayName: res.displayName, photoURL: res.photoURL });
+    });
+  }, []);
+
+  const signInWithGoogle = async () => {
+    try {
+      setLoading(true);
+      await signInWithPopup(auth, googleProvider)
+        .then((res) => {
+          setUser({displayName: res.user.displayName, photoURL: res.user.photoURL});
+          setLoading(false);
+        })
+        .catch((err) => console.log('google auth error', err));
+    } catch (err) {
+      console.error('google error', err);
+    }
+  };
 
   const signInWithFacebook = async () => {
     try {
@@ -15,22 +40,28 @@ function Auth() {
     }
   };
 
-  const signInWithGoogle = async () => {
-    try {
-      await signInWithPopup(auth, googleProvider)
-        .then((res) => console.log('google authprovider', res))
-        .catch((err) => console.log('google newschool error', err));
-    } catch (err) {
-      console.error('google oldschool error', err);
-    }
-  };
   return (
-    <div className="Auth">
-      <header className="Auth-header">
-        <button style={{background: "blue"}} onClick={signInWithFacebook}> Sign In With Facebook</button>
-        <button  style={{background: "green"}} onClick={signInWithGoogle}> Sign In With Google</button>
-      </header>
-    </div>
+    <>
+      {
+        loading ? 
+          <div className="loading">loading...</div>
+        :
+          <div className="menu">
+            {
+              user ?
+              <>
+                <img alt={user.displayName} src={user.photoURL} onClick={() => openMenu(s => !s)} />
+                {menu && <ul id="menu">
+                  <li onClick={(() => alert('click!'))}>{user.displayName}</li>
+                  <li onClick={(() => signOut(auth))}>Sign out</li>
+                </ul>}
+              </>
+              :
+              <button className="signInBtn" onClick={() => signInWithGoogle()}> Sign In With Google</button>
+            }
+          </div>
+      }
+    </>
   );
 }
 
