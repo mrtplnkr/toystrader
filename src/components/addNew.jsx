646feff2c99ef823355773bd0737ead7a4b1e405
martin <m.cepauskas@varutis.lt>
2023-07-23 21:16:02 +0300

    image upload works

--- src/components/addNew.jsx
+++ src/components/addNew.jsx
@@ -1,19 +1,62 @@
 
+import { useState } from "react";
+import { db, auth, storage } from "../firebase-config";
+import { ref, uploadBytes } from "firebase/storage";
+import { addDoc, collection } from "firebase/firestore";
+
 function AddNew() {
+    const [title, setTitle] = useState(String.empty);
+    const [fileUploadName, setFileUpload] = useState(null);
+    const toyCollectionRef = collection(db, "toys");
+
+    const uploadFile = async (file) => {
+        if (!file) return;
+        const filesFolderRef = ref(storage, `projectFiles/${file.name}`);
+        try {
+          await uploadBytes(filesFolderRef, file);
+          setFileUpload(file.name)
+        } catch (err) {
+          console.error(err);
+        }
+    };
+    
+    const onSubmitToy = async () => {
+        try {
+            await addDoc(toyCollectionRef, {
+                title,
+                file: fileUploadName,
+                userId: auth?.currentUser?.uid,
+            });
+        } catch (err) {
+            console.error(err);
+        }
+    };
+
+    // const fr = new FileReader();
+
+    // fr.onload = (e) => {
+    //     const { result } = e.target;
+    //     setFileUpload(result);
+    // }
+
     return (
       <>
         <h3>List your toy</h3>
 
-        <form id="newToy" style={{display: 'flex', flexDirection: 'column'}}>
-          <input id="name" type="text" style={{margin: '0.5em 0'}} />
-          <input id="picture" type="file" alt="toy" style={{margin: '0.5em 0'}} />
-          <input id="ageRecommendation" type="number" style={{margin: '0.5em 0', width: '2em'}} />
+        <form id="newToy" style={{display: 'flex', flexDirection: 'column'}} onSubmit={(e) => {
+            onSubmitToy();
+            e.preventDefault();
+        }}>
+            <input id="title" type="text" style={{margin: '0.5em 0'}} onChange={(e) => setTitle(e.target.value)} />
+            <input id="picture" type="file" alt="toy" style={{margin: '0.5em 0'}}
+                onChange={(e) => {
+                    uploadFile(e.target.files[0])
+                }} />
+
+            <div style={{display: 'flex', flexDirection: 'column'}}>
+              <button type="submit" style={{alignSelf: 'flex-end'}}>Add</button>
+            </div>
         </form>
-
-        <div style={{display: 'flex', flexDirection: 'column'}}>
-          <button style={{alignSelf: 'flex-end'}}>Save</button>
-        </div>
-
       </>
     );
 }
--- src/components/list.jsx
+++ src/components/list.jsx
@@ -1,13 +1,42 @@
+import { collection, getDocs } from "firebase/firestore";
+import { useState } from "react";
+import { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
+import { db } from "../firebase-config";
 
 function ListPage() {
     let navigate = useNavigate();
+
+    const [toyList, setToyList] = useState([]);
+
+    const moviesCollectionRef = collection(db, "toys");
+
+    useEffect(() => {
+      getMovieList();
+    }, []);
+    
+  const getMovieList = async () => {
+    try {
+      const data = await getDocs(moviesCollectionRef);
+      const filteredData = data.docs.map((doc) => ({
+        ...doc.data(),
+        id: doc.id,
+      }));
+      setToyList(filteredData);
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
+  console.log('toyList', toyList);
+
     return (
       <>
         <h3>All toys in your area</h3>
 
         <div style={{display: 'flex', flexDirection: 'column'}}>
           <button style={{alignSelf: 'flex-end'}} onClick={() => navigate('/addNew')}>add your toy</button>
+          <button style={{alignSelf: 'flex-start'}} onClick={() => getMovieList()}>refresh</button>
         </div>
 
         <ul id="toyList">
